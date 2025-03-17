2201daa32ffd6da1e96a1157fa434e3b6cec9416
PythonHacker24 <adityapatil24680@gmail.com>
2025-03-17 17:21:40 +0530

    updated types

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -4,13 +4,13 @@ export default function StatsSection() {
             <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-16">
                 <div className="relative z-10 mx-auto max-w-xl space-y-6 text-center">
                     <h2 className="text-4xl font-medium lg:text-5xl">Stockic AI Metrics for Maximum Value Per Article</h2>
-                    <p>Stockic keeps it's AI updated with the latest Open Source Models out there to provide better news summarization without bias</p>
+                    <p>Stockic keeps its AI updated with the latest Open Source Models out there to provide better news summarization without bias</p>
                 </div>
 
                 <div className="grid gap-12 divide-y *:text-center md:grid-cols-3 md:gap-2 md:divide-x md:divide-y-0">
                     <div className="space-y-4">
                         <div className="text-5xl font-bold">0.30</div>
-                        <p>Rogue-L Score</p>
+                        <p>ROGUE-L Score</p>
                         <p>0.7 more than Inshorts</p>
                     </div>
                     <div className="space-y-4">
