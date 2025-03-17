a7aeced5ca7e20e121bd6ac05ce87781ce8660be
PythonHacker24 <adityapatil24680@gmail.com>
2025-03-17 14:18:42 +0530

    fixed spelling

--- src/components/content-4.tsx
+++ src/components/content-4.tsx
@@ -9,7 +9,7 @@ export default function ContentSection() {
                 <div className="grid gap-6 md:grid-cols-2 md:gap-12">
                     <h2 className="text-4xl font-medium">Stockic maximizes your Experience with Better News and Premium UI</h2>
                     <div className="space-y-6">
-                        <p>Reading news from random sources is an utter waste of time. You are bombarded with information you might never need, consuming your cognative efforts which needs to go in better stuff.</p>
+                        <p>Reading news from random sources is an utter waste of time. You are bombarded with information you might never need, consuming your cognitive energy which needs to go in better stuff.</p>
                         <p>
                             Stockic <span className="font-bold">with it's AI capabilities and Powerful UI</span> provides all the insights you need complied into handful of sentences by removing unnecessary details and providing you the option to read full news article only if you want to.
                         </p>
